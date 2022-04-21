78d31e14359e9c60ed9f9a4b17e6154f1118a26e
kaoya1125 <103968875+kaoya1125@users.noreply.github.com>
2022-04-21 13:19:23 +0100

    Update betting.sol

--- betting.sol
+++ betting.sol
@@ -1,26 +1,5 @@
 pragma solidity 0.8.7;
 
-interface IBEP20 {
-  function totalSupply() external view returns (uint256);
-
-  function decimals() external view returns (uint8);
-
-  function symbol() external view returns (string memory);
-
-  function name() external view returns (string memory);
-
-  function getOwner() external view returns (address);
-
-  function balanceOf(address account) external view returns (uint256);
-
-  function transfer(address recipient, uint256 amount) external returns (bool);
-
-  function allowance(address _owner, address spender) external view returns (uint256);
-
-  function approve(address spender, uint256 amount) external returns (bool);
-
-  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-}
 interface IPriceOracle{
     function latestAnswer() external view returns (int256);
 }
@@ -120,7 +99,4 @@ contract betting{
         user.done = true;
         payable(msg.sender).transfer(userBalance);
     }
-    
-
-
 }
